959098fec35052cb013b5e1b4b856719ed75842c
naincyrani06 <naincyrani06it@gmail.com>
2020-08-10 23:08:55 +0530

    Storing the Result Into Dictionary

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,11 +1,26 @@
 #!/bin/bash -x
 echo "Welcome to Arithmetic Computation and Sorting"
-
+declare -A expressionResultStore
 
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
+expression1=`awk "BEGIN {print $a + ( $b * $c)}"`
+echo "a+b*c="$expression1
+expression2=`awk "BEGIN {print ($a * $b) + $c}"`
+echo "a*b+c="$expression2
+expression3=`awk "BEGIN {print $c + ($a / $b)}"`
+echo "c+a/b="$expression3
+expression4=`awk "BEGIN {print ($a % $b) + $c}"`
+echo "a%b+c="$expression4
+expressionResultStore[a+b*c]=$expression1
+expressionResultStore[a*b+c]=$expression2
+expressionResultStore[c+a/b]=$expression3
+expressionResultStore[a%b+c]=$expression4
+echo "Generated Dictionary:"
+for i in "${!expressionResultStore[@]}"
+do
+	echo "$i:${expressionResultStore[$i]}"
+done
 
-expression=`awk "BEGIN {print ($a % $b) + $c}"`
-echo "(a%b+c)="$expression
