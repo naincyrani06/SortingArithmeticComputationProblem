4116e9583f1f7a4f1abdec0a6e89ac9097b255e3
naincyrani06 <naincyrani06it@gmail.com>
2020-08-10 22:49:12 +0530

    Solving Expression (c+a/b)

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -5,5 +5,7 @@ echo "Welcome to Arithmetic Computation and Sorting"
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
-expression=`awk "BEGIN {print ($a * $b) + $c}"`
-echo "a*b+c="$expression
+
+
+expression=`awk "BEGIN {print $c + ($a / $b)}"`
+echo "c+a/b="$expression
