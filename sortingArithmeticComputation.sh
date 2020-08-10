faa641b6bfa85efeeaef08417800765eb705a155
naincyrani06 <naincyrani06it@gmail.com>
2020-08-10 22:35:48 +0530

    Solving Expression (a+b*c)

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -5,3 +5,5 @@ echo "Welcome to Arithmetic Computation and Sorting"
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
+expression=`awk "BEGIN {print $a + ( $b * $c)}"`
+echo "(a+b*c)="$expression
